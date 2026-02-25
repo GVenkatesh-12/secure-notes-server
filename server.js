cf475fae29e91cfa1cb356bc3e3dbb2520ed64ce
gvenkatesh-12 <gvenkatesh.on@gmail.com>
2026-02-25 09:51:25 +0530

    changed token expiry

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
 
 const blacklistSchema = new mongoose.Schema({
     token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: '30d' }
+    createdAt: { type: Date, default: Date.now, expires: 60 * 60 * 24 * 30 }
 });
 
 const Note = mongoose.model('Note', noteSchema);
